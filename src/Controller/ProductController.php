6152108e765095ff895df1ec7cdec94452bdfbee
AlexGthr <agauthier5@outlook.fr>
2024-05-31 15:44:06 +0200

    Ajout de la gestion d'un panier de produit Affichage du panier dans la page panier Ajout de la possibilité de supprimer un produit du panier

--- src/Controller/ProductController.php
+++ src/Controller/ProductController.php
@@ -4,8 +4,10 @@ namespace App\Controller;
 
 use App\Entity\Product;
 use App\Repository\ProductRepository;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\HttpFoundation\Session\SessionInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class ProductController extends AbstractController
@@ -48,12 +50,68 @@ class ProductController extends AbstractController
     }
 
     #[Route('/panier', name: 'app_panier')]
-    public function panier(): Response
+    public function panier(SessionInterface $session, ProductRepository $productRepository): Response
     {
+
+        $panier = $session->get('panier', []);
+
+        $panierWithData = [];
+
+        foreach($panier as $id => $quantity) {
+
+            $panierWithData[] = [
+                'product' => $productRepository->find($id),
+                'quantity' => $quantity
+            ];
+
+        }
+
+        $total = 0;
+
+        foreach($panierWithData as $item) {
+            $totalItem = $item['product']->getPrice() * $item['quantity'];
+            $total += $totalItem;
+        }
 
             return $this->render('product/panier.html.twig', [
                 'controller_name' => 'ProductController',
+                'items' => $panierWithData,
+                'total' => $total
             ]);
 
+
+    }
+
+    #[Route('/panier/add/{id}', name: 'app_addPanier')]
+    public function addPanier($id, SessionInterface $session): Response
+    {
+
+        $panier = $session->get('panier', []);
+
+        if (!empty($panier[$id])) {
+            $panier[$id]++;
+        } else {
+            $panier[$id] = 1;
+        }
+
+
+        $session->set('panier', $panier);
+
+        return $this->redirectToRoute('app_product');
+    }
+
+    #[Route('/panier/remove/{id}', name: 'app_removeItemPanier')]
+    public function removeItemPanier($id, SessionInterface $session): Response
+    {
+
+        $panier = $session->get('panier', []);
+
+        if (!empty($panier[$id])) {
+            unset($panier[$id]);
+        }
+
+        $session->set('panier', $panier);
+
+        return $this->redirectToRoute('app_panier');
     }
 }
