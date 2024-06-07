978dbbf6163c1c4d858e420aa2f6e0cf4a437c75
AlexGthr <agauthier5@outlook.fr>
2024-06-07 23:17:26 +0200

    Ajout d'un slide toggle sur la page à propos

--- public/js/script.js
+++ public/js/script.js
@@ -1,3 +1,12 @@
 // document.getElementById('burger-menu').addEventListener('click', () => {
 //     document.getElementById('nav-links').classList.toggle('active');
 // });
+
+// Quand le DOM est ready alors je crée une function :
+$(document).ready(function() {
+    // Qui crée un événement sur le click de la div ".question_faq"
+    $('.question_faq').click(function() {
+        // Qui va récupéré le parent de .question_faq (this) et slideToggle .answer_faq
+        $(this).parent('.faq').find('.answer_faq').slideToggle('slow');
+    });
+});
