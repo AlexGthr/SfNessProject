9eecdb3cbd7a79d0e6276b673c8bfe279faaec5d
AlexGthr <agauthier5@outlook.fr>
2024-06-13 23:05:01 +0200

    Modification du formulaire d'adresse sur la page profil : • Profil vue twig - Si l'utilisateur possède une adresse, j'affiche un formulaire avec ses informations déjà remplis - Sinon un formulaire vide • Controller - Récupération des informations du formulaire avec filtrages et condition regex/null - Modification de l'adresse si déjà existante - Création d'un champ adresse dans la BDD si aucune existante.

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -15,46 +15,83 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 class UserController extends AbstractController
 {
     #[Route('/profil/{id}', name: 'app_profil')]
-    public function index(UserRepository $userRepository, AddressRepository $addressRepository, Address $address = null, EntityManagerInterface $entityManager, Request $request, $id): Response
+    public function index(UserRepository $userRepository, $id): Response
     {
         $user = $this->getUser();
         $infoUser = $userRepository->find($id);
-        $address = $addressRepository->findBy(['users' => $id]);
 
-        if ($user && $infoUser && $user === $infoUser) { 
-
-            if (!$address) {
-                $address = new Address();
-            }
-
-            // On crée le formulaire pour le PRODUIT
-            $form = $this->createForm(AddressType::class, $address);
-
-            $form->handleRequest($request);
-
-            if ($form->isSubmitted() && $form->isValid()) {
-
-                $address = $form->getData();
-                $address->setUsers($infoUser);
-
-                $entityManager->persist($address);
-                $entityManager->flush();
+        if ($user && $infoUser && $user === $infoUser) {
 
             return $this->render('user/index.html.twig', [
-                'controller_name' => 'UserController',
-                'infoUser' => $infoUser,
-                'formProfil' => $form
-            ]);
-        }
-        return $this->render('user/index.html.twig', [
             'controller_name' => 'UserController',
-            'infoUser' => $infoUser,
-            'formProfil' => $form
-        ]);
-        } else {
-            return $this->redirectToRoute('app_home');
+            'infoUser' => $infoUser
+            ]);
+
         }
 
         return $this->redirectToRoute('app_home');
     }
+
+    #[Route('/addAddress/{id}', name: 'app_address')]
+    public function addAddress(UserRepository $userRepository, AddressRepository $addressRepository, Address $address = null, EntityManagerInterface $entityManager, Request $request, $id): Response
+    {
+        $user = $this->getUser();
+        $infoUser = $userRepository->find($id);
+
+        if ($user && $infoUser && $user === $infoUser) {
+
+            $address = $addressRepository->findOneBy(['users' => $id]);
+            // Récupération des données et filtrage
+            $nom = filter_var($request->request->get('nom'), FILTER_SANITIZE_STRING);
+            $prenom = filter_var($request->request->get('prenom'), FILTER_SANITIZE_STRING);
+            $adresse = filter_var($request->request->get('adresse'), FILTER_SANITIZE_STRING);
+            $codePostal = filter_var($request->request->get('code_postal'), FILTER_SANITIZE_STRING);
+            $ville = filter_var($request->request->get('ville'), FILTER_SANITIZE_STRING);            
+
+            // Expressions régulières pour les validations
+            $regexLetters = '/^[a-zA-Z]+$/';
+            $isNomValid = !empty($nom) && preg_match($regexLetters, $nom);
+            $isPrenomValid = !empty($prenom) && preg_match($regexLetters, $prenom);
+            $isAdresseValid = !empty($adresse);
+            $isCodePostalValid = !empty($codePostal);
+            $isVilleValid = !empty($ville) && preg_match($regexLetters, $ville);
+
+            if ($isNomValid && $isPrenomValid && $isAdresseValid && $isCodePostalValid && $isVilleValid) {
+
+                if ($address) {
+
+                    $address->setName($prenom);
+                    $address->setLastName($nom);
+                    $address->setAddress($adresse);
+                    $address->setZipCode($codePostal);
+                    $address->setCity($ville);
+                    
+                    $entityManager->persist($address);
+                    $entityManager->flush();
+
+                    return $this->redirectToRoute('app_profil', ['id' => $id]);
+
+                } else {
+                    $address = new Address();
+
+                    $address->setName($prenom);
+                    $address->setLastName($nom);
+                    $address->setAddress($adresse);
+                    $address->setZipCode($codePostal);
+                    $address->setCity($ville);
+                    $address->setUsers($infoUser);
+
+                    $entityManager->persist($address);
+                    $entityManager->flush();
+
+                    return $this->redirectToRoute('app_profil', ['id' => $id]);
+                }
+            } else {
+                return $this->redirectToRoute('app_profil', ['id' => $id]);
+            }
+        }
+
+        return $this->redirectToRoute('app_home');
+    }
+
 }
